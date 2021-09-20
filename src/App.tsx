82db5c0fa0aa224f1be6d7ebc3bffc48e502764f
西谷漠 <nishiya_baku@bakuNishiya.local>
2021-09-21 01:03:27 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
+  const morningTime = [8,9,10,11]
+  const afternoon = [12,13,14,15,16,17]
+  const endTime = [18,19,20,21,22,23]
+ 
+  const sampleDate = new Date() 
+  const sampleDate2 = sampleDate.getHours() 
+  const sampleDate3 = 18
+
+  const notify4 = (time: number) => {
+    if(morningTime.includes(time)){
+      toast("〇〇を投稿しました。今日も１日がんばりましょう！")
+    }else if(afternoon.includes(time)){
+      toast("〇〇を投稿しました。午後もがんばりましょう！")
+    }else if(endTime.includes(time)){
+      toast("〇〇を投稿しました。今日も１日お疲れさまでした！")
+    }else{
+      toast("〇〇を投稿しました")
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button onClick={() => notify4(sampleDate2)}>please Button notify4</button>
+      <ToastContainer />
       </header>
     </div>
   );
